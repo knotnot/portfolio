2d273c7e0d8e9f59b487811f5de476d66cbdc721
knotnot <witchayut.tang@gmail.com>
2025-05-17 18:24:38 +0700

    update & upload resume

--- script.js
+++
@@ -1,9 +0,0 @@
-function toggleDetails(activityId) {
-    const details = document.getElementById(`activity-details-${activityId}`);
-    if (details.style.display === "none" || details.style.display === "") {
-        details.style.display = "block";
-    } else {
-        details.style.display = "none";
-    }
-}
-
